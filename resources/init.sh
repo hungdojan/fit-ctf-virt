fc0705d148a9ec6ac7c087d1b014fae359b44ab4
Hung Do <hungdojan@gmail.com>
2026-02-04 22:32:18 +0100

    feat: Ansible configuration for Playground

--- resources/init.sh
+++ resources/init.sh
@@ -4,11 +4,10 @@ set -xe
 
 export PATH="$PATH:$HOME/.local/bin"
 echo "export PATH=\"$PATH:$HOME/.local/bin\"" >> "$HOME"/.bashrc
-#
-# dnf update
-# dnf install epel-release -y
-# dnf install -y git pipx neovim
-#
-# pipx install --include-deps ansible
-cd /tmp/setup/ansible
-ansible-playbook playbooks/main.yaml --extra-vars "@/tmp/setup/vals.yaml"
+
+dnf update -y
+dnf install epel-release -y
+dnf install -y git pipx neovim ansible-core
+
+ansible-galaxy collection install community.general
+ansible-galaxy collection install community.mongodb
